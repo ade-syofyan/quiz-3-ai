d10d521747de2cd062079b1876e1e4c98d3fc88f
Ade Syofyan <ade.syofyan@gmail.com>
2025-06-18 21:54:46 +0700

    Starter Project

---
+++ index.js
@@ -0,0 +1,15 @@
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({
+  apiKey: "AIzaSyAZBIqLVkWcZA6iPsSns4vovIH9YHEplqo",
+});
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: "Explain how AI works in a few words",
+  });
+  console.log(response.text);
+}
+
+await main();
